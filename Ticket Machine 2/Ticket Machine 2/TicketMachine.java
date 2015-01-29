9434b209c7823f157d1ca3401aa2b1bf99c44372
lagraham0324 <lagraham@mtu.edu>
2015-01-29 00:20:46 -0500

    Chapter 2 Part 2 Bookwork

---
+++ Ticket Machine 2/Ticket Machine 2/TicketMachine.java
@@ -0,0 +1,147 @@
+/**
+ * TicketMachine models a ticket machine that issues
+ * flat-fare tickets.
+ * The price of a ticket is specified via the constructor.
+ * Instances will check to ensure that a user only enters
+ * sensible amounts of money, and will only print a ticket
+ * if enough money has been input.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2011.07.31
+ */
+public class TicketMachine
+{
+    // The price of a ticket from this machine.
+    private double price;
+    // The amount of money entered by a customer so far.
+    private double balance;
+    // The total amount of money collected by this machine.
+    private double total;
+    // The discounted price.
+    private double newPrice;
+
+    /**
+     * Create a machine that issues tickets of the given price.
+     */
+    public TicketMachine(double cost)
+    {
+        price = cost;
+        balance = 0;
+        total = 0;
+    }
+
+    /**
+     * @Return The price of a ticket.
+     */
+    public double getPrice()
+    {
+        return price;
+    }
+
+    /**
+     * Return The amount of money already inserted for the
+     * next ticket.
+     */
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    /**
+     * Receive an amount of money from a customer.
+     * Check that the amount is sensible.
+     */
+    public void insertMoney(double amount)
+    {
+        if(amount > 0) {
+            balance = balance + amount;
+        }
+        else {
+            System.out.println("Use a positive amount rather than: " +
+                               amount);
+        }
+    }
+    
+    /**
+     * Offers price at a 50% discount
+     */
+    public double getDiscount()
+    {
+        newPrice = price*.5;
+        return newPrice;
+    }
+
+    /**
+     * Print a ticket if enough money has been inserted, and
+     * reduce the current balance by the ticket price. Print
+     * an error message if more money is required.
+     */
+    public void printTicket()
+    {
+        if(balance >= price) 
+        {
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + price;
+            // Reduce the balance by the prince.
+            balance = balance - price;
+        }
+        else if(balance >= newPrice)
+        {
+             // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + newPrice + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + newPrice;
+            // Reduce the balance by the prince.
+            balance = balance - newPrice;
+        }
+        else 
+        {
+            if(balance < newPrice)
+            {
+            
+                System.out.println("You must insert at least: " +
+                               (newPrice - balance) + " more cents.");
+            }
+            else
+            {
+             System.out.println("You must insert at least: " +
+                               (price - balance) + " more cents.");   
+            }
+                    
+        }
+    }
+
+    /**
+     * Return the money in the balance.
+     * The balance is cleared.
+     */
+    public double refundBalance()
+    {
+        double amountToRefund;
+        amountToRefund = balance;
+        balance = 0;
+        return amountToRefund;
+    }
+    
+    public double emptyMachine()
+    {
+     double totalMoney;
+     totalMoney = total;
+     total = 0;
+     return totalMoney;
+    }
+}
